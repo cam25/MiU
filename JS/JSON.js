757d01f3426f7e2ba1129127e5c0048d316f6d1f
Cameron Mozie <cam25@fullsail.edu>
2012-06-25 21:43:24 -0400

    setup

---
+++ JS/JSON.js
@@ -0,0 +1,357 @@
+var json = {
+	"event1": {
+		"group": ["Event:", "Wedding"],
+		"firstName":["FirstName:", "John"],
+		"lastName": ["LastName:", "Smith"],
+		"address": ["address:", "333 West 3rd St"],
+		"city":["City:","Baltimore"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","410-555-5555"],
+		"email":["Email:", "email@email.com"],
+		"timeEVent":["TimeOfEvent:", "12:00PM"],
+		"date":["DateOfEvent:", "12/24/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "6"],
+		"checkBox":["CheckBox:", "Monday"]
+		
+		
+	},
+	"event2": {
+		"group": ["Event:", "Birthday"],
+		"firstName":["FirstName:", "Carl"],
+		"lastName": ["LastName:", "Thompson"],
+		"address": ["address:", "1710 Baltimore Ave"],
+		"city":["City:","Laurel"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","301-555-5555"],
+		"email":["Email:", "email@email.com"],
+		"timeEVent":["TimeOfEvent:", "2:00PM"],
+		"date":["DateOfEvent:", "1/11/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "10"],
+		"checkBox":["CheckBox:", "Tuesday"]
+		
+		
+	},
+	"event3": {
+		"group": ["Event:", "Meeting"],
+		"firstName":["FirstName:", "Tom"],
+		"lastName": ["LastName:", "Jones"],
+		"address": ["address:", "1330 St Paul St."],
+		"city":["City:","Salisbury"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","410-222-5555"],
+		"email":["Email:", "email@email.com"],
+		"timeEVent":["TimeOfEvent:", "8:00PM"],
+		"date":["DateOfEvent:", "09/10/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "6"],
+		"checkBox":["CheckBox:", "Saturday"]
+		
+		
+	},
+	"event4": {
+		"group": ["Event:", "BabyShower"],
+		"firstName":["FirstName:", "Clarence"],
+		"lastName": ["LastName:", "Stewart"],
+		"address": ["address:", "222 Pearl St"],
+		"city":["City:","Bowie"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","301-111-5555"],
+		"email":["Email:", "email@bowie.com"],
+		"timeEVent":["TimeOfEvent:", "9:00PM"],
+		"date":["DateOfEvent:", "06/22/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "6"],
+		"checkBox":["CheckBox:", "Monday"]
+		
+		
+	},
+	"event5": {
+		"group": ["Event:", "Birthday"],
+		"firstName":["FirstName:", "Wallace"],
+		"lastName": ["LastName:", "Padd"],
+		"address": ["address:", "213 Clover St."],
+		"city":["City:","New Carrolton"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","301-433-5555"],
+		"email":["Email:", "email@NewCarrol.com"],
+		"timeEVent":["TimeOfEvent:", "8:30PM"],
+		"date":["DateOfEvent:", "10/10/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "6"],
+		"checkBox":["CheckBox:", "Thursday"]
+		
+		
+	},
+	
+	"event6": {
+		"group": ["Event:", "Wedding"],
+		"firstName":["FirstName:", "Terrence"],
+		"lastName": ["LastName:", "Wilks"],
+		"address": ["address:", "330 Landover Rd"],
+		"city":["City:","Landover"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","301-222-5555"],
+		"email":["Email:", "email@Landover.com"],
+		"timeEVent":["TimeOfEvent:", "1:00PM"],
+		"date":["DateOfEvent:", "12/10/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "6"],
+		"checkBox":["CheckBox:", "Tuesday"]
+		
+		
+	},
+	
+	"event7": {
+		"group": ["Event:", "Meeting"],
+		"firstName":["FirstName:", "Warner"],
+		"lastName": ["LastName:", "Jones"],
+		"address": ["address:", "443 W Palm Pl."],
+		"city":["City:","Ocean City"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","443-334-5555"],
+		"email":["Email:", "email@OC.com"],
+		"timeEVent":["TimeOfEvent:", "8:30AM"],
+		"date":["DateOfEvent:", "07/10/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "8"],
+		"checkBox":["CheckBox:", "Friday"]
+		
+		
+	},
+	
+	"event8": {
+		"group": ["Event:", "BabyShower"],
+		"firstName":["FirstName:", "Marie"],
+		"lastName": ["LastName:", "Henks"],
+		"address": ["address:", "1323 Blank st."],
+		"city":["City:","Waldorf"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","301-551-5551"],
+		"email":["Email:", "email@Wallys.com"],
+		"timeEVent":["TimeOfEvent:", "4:00PM"],
+		"date":["DateOfEvent:", "11/10/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "6"],
+		"checkBox":["CheckBox:", "Sunday"]
+		
+		
+	},
+	
+	"event9": {
+		"group": ["Event:", "Wedding"],
+		"firstName":["FirstName:", "Rebecca"],
+		"lastName": ["LastName:", "Toms"],
+		"address": ["address:", "443 Annapolis Rd."],
+		"city":["City:","Lanham"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","240-333-5555"],
+		"email":["Email:", "email@Lapes.com"],
+		"timeEVent":["TimeOfEvent:", "1:00PM"],
+		"date":["DateOfEvent:", "12/23/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "6"],
+		"checkBox":["CheckBox:", "Saturday"]
+		
+		
+	},
+	"event10": {
+		"group": ["Event:", "Meeting"],
+		"firstName":["FirstName:", "West"],
+		"lastName": ["LastName:", "Ucly"],
+		"address": ["address:", "122 West East st"],
+		"city":["City:","Glen Burnie"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","410-332-3333"],
+		"email":["Email:", "Westter@email.com"],
+		"timeEVent":["TimeOfEvent:", "6:30AM"],
+		"date":["DateOfEvent:", "10/10/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "6"],
+		"checkBox":["CheckBox:", "Saturday"]
+		
+		
+	},
+	"event11": {
+		"group": ["Event:", "Concert"],
+		"firstName":["FirstName:", "Morris"],
+		"lastName": ["LastName:", "Party"],
+		"address": ["address:", "223 Lucid st"],
+		"city":["City:","Severna Park"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","410-332-2223"],
+		"email":["Email:", "Martry@email.com"],
+		"timeEVent":["TimeOfEvent:", "10:30PM"],
+		"date":["DateOfEvent:", "07/10/2012"],
+		"textBox":["TextBox:", "Text Here"],
+		"iq":["Range:", "6"],
+		"checkBox":["CheckBox:", "Saturday"]
+		
+		
+	},
+	"event12": {
+		"group": ["Event:", "Movies"],
+		"firstName":["FirstName:", "Stephen"],
+		"lastName": ["LastName:", "Bartely"],
+		"address": ["address:", "2231 Barter st"],
+		"city":["City:","Alexandria"],
+		"state":["State:","Virginia"],
+		"phoneNumber":["PhoneNumber:","703-332-2223"],
+		"email":["Email:", "Virginny@email.com"],
+		"timeEVent":["TimeOfEvent:", "11:00PM"],
+		"date":["DateOfEvent:", "07/10/2012"],
+		"textBox":["TextBox:", "Spiderman 3"],
+		"iq":["Range:", "10"],
+		"checkBox":["CheckBox:", "Friday"]
+		
+		
+	},
+	"event13": {
+		"group": ["Event:", "KidsEvent"],
+		"firstName":["FirstName:", "Charlie"],
+		"lastName": ["LastName:", "Monasst"],
+		"address": ["address:", "303 Worden st"],
+		"city":["City:","Fairfax"],
+		"state":["State:","Virginia"],
+		"phoneNumber":["PhoneNumber:","703-112-2223"],
+		"email":["Email:", "Stomach@email.com"],
+		"timeEVent":["TimeOfEvent:", "2:00PM"],
+		"date":["DateOfEvent:", "07/10/2012"],
+		"textBox":["TextBox:", "Chuck E Cheese"],
+		"iq":["Range:", "10"],
+		"checkBox":["CheckBox:", "Saturday"]
+		
+		
+	},
+	"event14": {
+		"group": ["Event:", "DinnerParty"],
+		"firstName":["FirstName:", "Marie"],
+		"lastName": ["LastName:", "Swanson"],
+		"address": ["address:", "1 Flowers Rd"],
+		"city":["City:","Silver Spring"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","301-431-1234"],
+		"email":["Email:", "Storend@email.com"],
+		"timeEVent":["TimeOfEvent:", "8:00PM"],
+		"date":["DateOfEvent:", "11/14/2012"],
+		"textBox":["TextBox:", "Call Ahead Of Time."],
+		"iq":["Range:", "8"],
+		"checkBox":["CheckBox:", "Wednesday"]
+		
+		
+	},
+	"event15": {
+		"group": ["Event:", "RoadTrip"],
+		"firstName":["FirstName:", "Norman"],
+		"lastName": ["LastName:", "Brown"],
+		"address": ["address:", "12 Ward St"],
+		"city":["City:","Greenbelt"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","301-431-1234"],
+		"email":["Email:", "Saven@email.com"],
+		"timeEVent":["TimeOfEvent:", "5:00AM"],
+		"date":["DateOfEvent:", "12/14/2012"],
+		"textBox":["TextBox:", "Call Norman Before We leave."],
+		"iq":["Range:", "8"],
+		"checkBox":["CheckBox:", "Friday"]
+		
+		
+	},
+	"event16": {
+		"group": ["Event:", "SportingEvent"],
+		"firstName":["FirstName:", "Paul"],
+		"lastName": ["LastName:", "Norman"],
+		"address": ["address:", "333 Corin Rd"],
+		"city":["City:","Seabrook"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","301-239-1234"],
+		"email":["Email:", "Goode@email.com"],
+		"timeEVent":["TimeOfEvent:", "6:00PM"],
+		"date":["DateOfEvent:", "07/12/2012"],
+		"textBox":["TextBox:", "Orioles Game."],
+		"iq":["Range:", "8"],
+		"checkBox":["CheckBox:", "Friday"]
+		
+		
+	},
+	"event17": {
+		"group": ["Event:", "BookSigning"],
+		"firstName":["FirstName:", "Wester"],
+		"lastName": ["LastName:", "Borina"],
+		"address": ["address:", "2300 Port St"],
+		"city":["City:","Brooklyn"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","443-229-2233"],
+		"email":["Email:", "Mine@email.com"],
+		"timeEVent":["TimeOfEvent:", "4:00PM"],
+		"date":["DateOfEvent:", "09/22/2012"],
+		"textBox":["TextBox:", "Favorite Book"],
+		"iq":["Range:", "8"],
+		"checkBox":["CheckBox:", "Friday"]
+		
+		
+	},
+	"event18": {
+		"group": ["Event:", "DinnerParty"],
+		"firstName":["FirstName:", "Pal"],
+		"lastName": ["LastName:", "Newton"],
+		"address": ["address:", "2920 Pernice Rd"],
+		"city":["City:","Silver Spring"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","301-098-1234"],
+		"email":["Email:", "Valent@email.com"],
+		"timeEVent":["TimeOfEvent:", "7:30PM"],
+		"date":["DateOfEvent:", "11/14/2012"],
+		"textBox":["TextBox:", "Call pal when at diner."],
+		"iq":["Range:", "8"],
+		"checkBox":["CheckBox:", "Tuesday"]
+		
+		
+	},
+	"event19": {
+		"group": ["Event:", "Concert"],
+		"firstName":["FirstName:", "Nate"],
+		"lastName": ["LastName:", "Reynolds"],
+		"address": ["address:", "677 Roges Avenue"],
+		"city":["City:","Baltimore"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","410-331-1234"],
+		"email":["Email:", "Bait@email.com"],
+		"timeEVent":["TimeOfEvent:", "10:00PM"],
+		"date":["DateOfEvent:", "01/14/2013"],
+		"textBox":["TextBox:", "50 Cent Show!"],
+		"iq":["Range:", "8"],
+		"checkBox":["CheckBox:", "Friday"]
+		
+		
+	},
+	"event20": {
+		"group": ["Event:", "KidsEvent"],
+		"firstName":["FirstName:", "Kerry"],
+		"lastName": ["LastName:", "Diggs"],
+		"address": ["address:", "3212 Harp St"],
+		"city":["City:","Baltimore"],
+		"state":["State:","Maryland"],
+		"phoneNumber":["PhoneNumber:","443-443-4443"],
+		"email":["Email:", "Balto@email.com"],
+		"timeEVent":["TimeOfEvent:", "6:00PM"],
+		"date":["DateOfEvent:", "02/14/2013"],
+		"textBox":["TextBox:", "Six Flags"],
+		"iq":["Range:", "8"],
+		"checkBox":["CheckBox:", "Saturday"]
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ JS/main.js
@@ -0,0 +1,482 @@
+//Project 4    
+//Cameron Mozie
+//Visual Framworks Week4 Project 4
+//VFW1206
+window.addEventListener("DOMContentLoaded", function () {
+
+
+    //getElementByIdFunction
+    function $(x) {
+        var theElement = document.getElementById(x);
+        return theElement;
+    }
+
+    //create select field element and populate with options.
+    function makeCats() {
+        var eventGroups = ["--Choose A Catagory--", "Birthday", "Wedding", "Meeting", "BabyShower","Concert","Movies","KidsEvent","DinnerParty","RoadTrip","SportingEvent","BookSigning"];
+        var formTag = document.getElementsByTagName("form"),
+            //form tag is array 
+            selectLi = $("chooseCat"),
+            makeSelect = document.createElement("select");
+        makeSelect.setAttribute("id", "groups");
+        for (var i = 0, j = eventGroups.length; i < j; i++) {
+            var makeOption = document.createElement("option");
+            var optText = eventGroups[i];
+            makeOption.setAttribute("value", optText);
+            makeOption.innerHTML = optText;
+            makeSelect.appendChild(makeOption);
+        }
+        selectLi.appendChild(makeSelect);
+
+    }
+
+    function getCheckboxValue() {
+
+        var checkBoxes = [];
+
+        if ($("Monday").checked) {
+            checkBoxes.push($("Monday").value);
+        }
+
+        if ($("Tuesday").checked) {
+            checkBoxes.push($("Tuesday").value);
+        }
+
+        if ($("Wednesday").checked) {
+            checkBoxes.push($("Wednesday").value);
+        }
+
+        if ($("Thursday").checked) {
+            checkBoxes.push($("Thursday").value);
+        }
+
+        if ($("Friday").checked) {
+            checkBoxes.push($("Friday").value);
+        }
+
+        if ($("Saturday").checked) {
+            checkBoxes.push($("Saturday").value);
+        }
+
+        if ($("Sunday").checked) {
+            checkBoxes.push($("Sunday").value);
+        }
+
+        return checkBoxes;
+    }
+
+    function toggleControls(tag) {
+        switch (tag) {
+        case "on":
+            $("eventForm").style.display = "none";
+            $("clearStoredData").style.display = "inline";
+            $("displayStoredData").style.display = "none";
+            $("addNew").style.display = "inline";
+            break;
+        case "off":
+            $("eventForm").style.display = "block";
+            $("clearStoredData").style.display = "inline";
+            $("displayStoredData").style.display = "inline";
+            $("addNew").style.display = "none";
+
+            break;
+        default:
+            return false;
+
+
+        }
+    }
+
+
+
+    function storeData(key) {
+        //IF there is no key, this means this is a brand new item we need a new key.
+        if (!key) {
+
+
+            var id = Math.floor(Math.random() * 1000000001);
+        } else {
+            //Set the id to the existing key we're editting so that it will save the data.
+            //The key is the same key that's been passed along from the editSubmit eventhanbdler.
+            //to the validate function, then passed here, into the storeData 
+            id = key;
+
+        }
+        //Gather up all our form field values and store in an object.
+        //Object properties contain array with the form label and input values
+        var item = {};
+        item.group = ["Event:", $("groups").value];
+        item.firstName = ["FirstName:", $("firstName").value];
+        item.lastName = ["LastName:", $("lastName").value];
+        item.address = ["address:", $("address").value];
+        item.city = ["City:", $("city").value];
+        item.state = ["State:", $("state").value];
+        item.phoneNumber = ["PhoneNumber:", $("phoneNumber").value];
+        item.email = ["Email:", $("email").value];
+        item.timeEVent = ["TimeOfEvent:", $("timeOfEvent").value];
+        item.date = ["DateOfEvent:", $("dateOfEvent").value];
+        item.textBox = ["TextBox:", $("textBox").value];
+        item.iq = ["Range:", $("range").value];
+        item.checkBox = ["CheckBox:", getCheckboxValue()];
+
+
+        //save data to local storage: Use Stringify to convert our object to a string.
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("Contact Saved");
+
+
+    }
+
+
+    function autoFillData() {
+        //The actual JSON object data required for this to work is coming from our json.js file. which is loaded from our addItem.html file.
+        //Store the JSON OBJECT in local storage.
+        for (var n in json) {
+            var id = Math.floor(Math.random() * 1000000001);
+            localStorage.setItem(id, JSON.stringify(json[n]));
+
+        }
+
+    }
+
+    function editItem() {
+        //Grab the data from our item from local storage.
+        var value = localStorage.getItem(this.key);
+        var item = JSON.parse(value);
+        var saveLink = $("saveEvent");
+        //shows the form
+        toggleControls("off");
+
+        //populate the form files with the current localStorage values
+        $("groups").value = item.group[1];
+        $("firstName").value = item.firstName[1];
+        $("lastName").value = item.lastName[1];
+        $("address").value = item.address[1];
+        $("city").value = item.city[1];
+        $("state").value = item.state[1];
+        $("phoneNumber").value = item.phoneNumber[1];
+        $("email").value = item.email[1];
+        $("timeOfEvent").value = item.timeEVent[1];
+        $("dateOfEvent").value = item.date[1];
+        $("textBox").value = item.textBox[1];
+        $("range").value = item.iq[1];
+
+        if ($("Monday").value == item.checkBox[1]) {
+	        $("Monday").setAttribute("checked", "checked");
+	        }
+        
+
+        if ($("Tuesday").value == item.checkBox[1]) {
+            $("Tuesday").setAttribute("checked", "checked");
+        }
+        if ($("Wednesday").value == item.checkBox[1]) {
+            $("Wednesday").setAttribute("checked", "checked");
+        }
+        if ($("Thursday").value == item.checkBox[1]) {
+            $("Thursday").setAttribute("checked", "checked");
+        }
+        if ($("Friday").value == item.checkBox[1]) {
+            $("Friday").setAttribute("checked", "checked");
+        }
+
+        if ($("Saturday").value == item.checkBox[1]) {
+            $("Saturday").setAttribute("checked", "checked");
+        }
+
+        if ($("Sunday").value == item.checkBox[1]) {
+            $("Sunday").setAttribute("checked", "checked");
+        }
+
+        //remove the initial listener from the input "save contact"       
+        saveLink.removeEventListener("click", storeData);
+        //change submit button value to edit button
+        $("saveEvent").value = "Edit Contact";
+        var editSubmit = $("saveEvent");
+        //save the key value established in this function as a property of the edit Submit event
+        //editSubmit.addEventListener("click", validate);
+        editSubmit.key = this.key;
+
+
+
+    }
+
+
+    function deleteItem() {
+        var ask = confirm("Are you sure you want to delete this event?");
+        if (ask) {
+            localStorage.removeItem(this.key);
+            window.location.reload();
+        } else {
+            alert("Event was NOT removed");
+
+        }
+
+    }
+
+    function makeItemLinks(key, linksLi) {
+        //add edit single item link
+        var editLink = document.createElement("a");
+        editLink.href = "#";
+        editLink.key = key;
+        var editText = "Edit Event";
+        editLink.addEventListener("click", editItem);
+        editLink.innerHTML = editText;
+        linksLi.appendChild(editLink);
+
+        //add line break
+        var breakTag = document.createElement("br");
+        linksLi.appendChild(breakTag);
+
+
+        //delete link
+        var deleteLink = document.createElement("a");
+        deleteLink.href = "#";
+        deleteLink.key = key;
+        var deleteText = "Delete Event";
+        deleteLink.addEventListener("click", deleteItem);
+        deleteLink.innerHTML = deleteText;
+        linksLi.appendChild(deleteLink);
+
+    }
+
+    function getImage(catName, makeOtherList) {
+        var imageLi = document.createElement("li");
+        makeOtherList.appendChild(imageLi);
+        var newImage = document.createElement("img");
+        var setSource = newImage.setAttribute("src", "Images/" + catName + ".png");
+        imageLi.appendChild(newImage);
+
+    }
+
+    function getData() {
+        toggleControls("on");
+        if (localStorage.length === 0) {
+            alert("There is no data inside Local Storage so default data was added.");
+            autoFillData();
+        }
+        //write Data from Local Storage to the browser.
+        var makeDiv = document.createElement("div");
+        makeDiv.setAttribute("id", "items");
+        var makeList = document.createElement("ul");
+        makeDiv.appendChild(makeList);
+        document.body.appendChild(makeDiv);
+        $("items").style.display = "block";
+        for (var i = 0, len = localStorage.length; i < len; i++) {
+            var makeLi = document.createElement("li");
+            var linksLi = document.createElement("li");
+            makeList.appendChild(makeLi);
+            var key = localStorage.key(i);
+            var value = localStorage.getItem(key);
+            //convert string from local storage value to an object by using json.Parse
+            var obj = JSON.parse(value);
+            var makeOtherList = document.createElement("ul");
+            makeLi.appendChild(makeOtherList);
+            getImage(obj.group[1], makeOtherList);
+            for (var tag in obj) {
+                var makeOtherLi = document.createElement("li");
+                makeOtherList.appendChild(makeOtherLi);
+                var optSubText = obj[tag][0] + " " + obj[tag][1];
+                makeOtherLi.innerHTML = optSubText;
+                makeOtherList.appendChild(linksLi);
+            }
+            makeItemLinks(localStorage.key(i), linksLi); // create our edit and delete buttons/links for each item in local storage
+        }
+
+    }
+
+    //get image for right catagory being displayed.
+
+    function clearLocal() {
+        if (localStorage.length === 0) {
+            alert("There is no data to clear.");
+        } else {
+            localStorage.clear();
+            alert("All contacts are deleted!");
+            window.location.reload();
+            return false;
+        }
+    }
+    
+    function validate(event) {
+        //define elements we want to check
+        var getGroup = $("groups");
+        var getFname = $("firstName");
+        var getLname = $("lastName");
+        var getPhoneNumber = $("phoneNumber");
+        var getEmail = $("email");
+        var getTime = $("timeOfEvent");
+        var getDate = $("dateOfEvent");
+        var getText = $("textBox");
+        var getIq = $("range");
+
+        //Reset Error Messages
+        var errorMsg = $("errors");
+        errorMsg.innerHTML = "";
+        getGroup.style.border = "1px solid black";
+        getFname.style.border = "1px solid black";
+        getLname.style.border = "1px solid black";
+        getPhoneNumber.style.border = "1px solid black";
+        getEmail.style.border = "1px solid black";
+        getTime.style.border = "1px solid black";
+        getDate.style.border = "1px solid black";
+
+        //get  Error messages
+        var messageArray = [];
+        //group validate
+        if (getGroup.value === "--Choose A Catagory--") {
+            var groupError = "Please Select A Group.";
+            getGroup.style.border = "1px solid red";
+            messageArray.push(groupError);
+
+        }
+
+        //firstName validation
+        if (getFname.value === "") {
+            var fNameError = "Please Enter A First Name.";
+            getFname.style.border = "1px solid red";
+            messageArray.push(fNameError);
+
+        }
+
+        if (getLname.value === "") {
+            var LnameError = "Please Enter A Last Name.";
+            getLname.style.border = "1px solid red";
+            messageArray.push(LnameError);
+        }
+
+
+        var RegPhone = /^\(?([0-9]{3})\)?[\-. ]?([0-9]{3})[\-. ]?([0-9]{4})$/;
+
+        if (!(RegPhone.exec(getPhoneNumber.value))) {
+            var phoneError = "Please Enter A valid Phone Number.";
+            getPhoneNumber.style.border = "1px solid red";
+            messageArray.push(phoneError);
+        }
+
+        var RegEx = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+
+        if (!(RegEx.exec(getEmail.value))) {
+            var emailError = "Please Enter A Valid Email Address.";
+            getEmail.style.border = "1px solid red";
+            messageArray.push(emailError);
+        }
+
+        var re2 = /^\d{1,2}:\d{2}([AP]M)?$/;
+
+        if (!(re2.exec(getTime.value))) {
+            var timeError = "Please Enter Valid Time.(ie 12:00PM)";
+            getTime.style.border = "1px solid red";
+            messageArray.push(timeError);
+        }
+
+        if (getDate.value === "") {
+            var dateError = "Please Enter A Date.(ie 12/24/2012)";
+            getDate.style.border = "1px solid red";
+            messageArray.push(dateError);
+        }
+
+        if (messageArray.length >= 1) {
+            for (var i = 0, j = messageArray.length; i < j; i++) {
+                var txt = document.createElement("li");
+                txt.innerHTML = messageArray[i];
+                errorMsg.appendChild(txt);
+
+            }
+            event.preventDefault();
+
+        } else {
+            //If all is OK this will save.
+            storeData(this.key);
+
+        }
+
+    }
+
+    function getSearch() {
+        var category = $("groups").value;
+        var term = $("search").value;
+        var makeDiv2 = document.createElement("div");
+        makeDiv2.setAttribute("id", "searches");
+        var makeList2 = document.createElement("ul");
+        makeDiv2.appendChild(makeList2);
+        document.body.appendChild(makeDiv2);
+        //By Category Only
+        if (category !== "--Choose A Catagory--" && term === "") {
+            for (var i = 0, j = localStorage.length; i < j; i++) {
+                var key = localStorage.key(i);
+                var vals = localStorage.getItem(key);
+                var items = JSON.parse(vals);
+                var catLi = document.createElement("li");
+                makeList2.appendChild(catLi);
+                if (category === items.group[1]) {
+                    for (var n in items) {
+                    var subCatLi = document.createElement("li");
+                    var subText = items[n][0] + "  " + items[n][1];
+                    makeList2.innerHTML = subText;
+                    
+                    
+                       
+                    }
+
+
+                }
+
+            }
+        }
+
+        //By term
+        if (term !== "" && category === "--Choose A Catagory--") {
+            for (i = 0, j = localStorage.length; i < j; i++) {
+                var key = localStorage.key(i);
+                var vals = localStorage.getItem(key);
+                var itemz = JSON.parse(vals);
+                var sublist = document.createElement("li")
+                for (n in itemz) {
+                    if (itemz[n][1].indexOf(term) != -1) {
+                        for (var q in itemz) {
+                            console.log(itemz[q][1]);
+                            console.log()
+                           
+                            
+                        }
+                    }
+                }
+            }
+
+        }
+
+        //By category and term
+        if (term !== "" && category !== "--Choose A Catagory--") {
+            for (i = 0, j = localStorage.length; i < j; i++) {
+                var key = localStorage.key(i);
+                var vals = localStorage.getItem(key);
+                var itemz = JSON.parse(vals);
+                for (n in itemz) {
+                    if (itemz[n][1].indexOf(term) != -1) {
+                        for (q in itemz) {
+                            console.log(itemz[q][1]);							
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+
+
+    makeCats();
+
+    var displayLink = $("displayStoredData");
+    displayLink.addEventListener("click", getData);
+    var clearLink = $("clearStoredData");
+    clearLink.addEventListener("click", clearLocal);
+    var saveLink = $("saveEvent");
+    saveLink.addEventListener("click", validate);
+    var editSubmit = $("saveEvent");
+    editSubmit.addEventListener("click", validate);
+    var search = $("searchButton");
+    search.addEventListener("click", getSearch);
+
+
+});
